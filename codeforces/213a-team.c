560a24ba3e6648305d27d56b8cccedeafe77115f
JDSeiler <jorsei1p217@gmail.com>
2020-11-27 17:19:25 -0500

    Solution for 213a-team

---
+++ codeforces/213a-team.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// Today I Learned: Getting the size of an array is... difficult.. in C
+int sum(int arr[], int size) {
+    int acc = 0;
+    for (int i=0; i<size; i++) {
+        acc += arr[i];
+    }
+    return acc;
+}
+
+void main(void) {
+    int num_problems;
+    scanf("%d", &num_problems);
+
+    int problems[num_problems][3];
+
+    for (int i=0; i<num_problems; i++) {
+        scanf("%d %d %d", &(problems[i][0]), &(problems[i][1]), &(problems[i][2]));
+    }
+
+    int will_solve = 0;
+    for (int i=0; i<num_problems; i++) {
+        if (sum(problems[i], 3) >= 2) {
+            will_solve++;
+        }
+    }
+    printf("%d\n", will_solve);
+}
